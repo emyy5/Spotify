c1d5713fcf8a02267df4d458d8455d25e611ab25
Github <emyy5>
2023-10-12 04:48:40 +0300

    AuthManager

--- SpotifyApp/Controllers/Other/AuthViewController.swift
+++ SpotifyApp/Controllers/Other/AuthViewController.swift
@@ -45,7 +45,14 @@ class AuthViewController: UIViewController,WKNavigationDelegate{
         else{
             return
         }
+        webView.isHidden = true
         print("Code:\(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code){ [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+        }
     }
 
 }
--- SpotifyApp/Managers/AuthManager.swift
+++ SpotifyApp/Managers/AuthManager.swift
@@ -12,6 +12,7 @@ final class AuthManager {
     struct Constants {
         static let clientID = "e2661df1c0764416b4fc5969163edd80"
         static let clientSecret = "0c90f645004f4ba89d878ebe22033725"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     var isSignedIn: Bool{
         return false
@@ -40,6 +41,44 @@ final class AuthManager {
     
     ){
         //get Token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        var components = URLComponents()
+        components.queryItems = [
+        URLQueryItem(name: "grant_type", value: "authorizaton_code"),
+        URLQueryItem(name: "code", value: code),
+        URLQueryItem(name: "redirect_uri", value: "https://www.iosacademy.io"),
+        ]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded ", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Faliure to get base64")
+            completion(false)
+            return
+        }
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request){ data, _, error in
+            
+            guard let  data = data,
+                  error == nil else {
+                completion(false)
+                return
+            }
+            do{
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("Success: \(json)")
+            }
+            catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
     }
     public func refreshAccessToken(){
         
